8d3c4928a59e4fe3b9180cb7dd8042d0e6aa8b02
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-09-26 10:44:52 +0200

    fix: handle version at package level

---
+++ imgsync/__init__.py
@@ -0,0 +1,22 @@
+"""Glance imgsync tool."""
+
+from contextlib import suppress
+import importlib.metadata
+import pathlib
+
+
+__version__ = "1.3.0"
+
+
+def extract_version() -> str:
+    """Return either the version of the package installed."""
+    with suppress(FileNotFoundError, StopIteration):
+        root_dir = pathlib.Path(__file__).parent.parent.parent
+        with open(root_dir / "pyproject.toml", encoding="utf-8") as pyproject_toml:
+            version = (
+                next(line for line in pyproject_toml if line.startswith("version"))
+                .split("=")[1]
+                .strip("'\"\n ")
+            )
+            return f"{version}-dev (at {root_dir})"
+    return importlib.metadata.version(__package__ or __name__.split(".", maxsplit=1)[0])
